8bf9b36f7749c305138f874f55fdfc1754c751e7
Aleksander Jaworski <aleck156@gmail.com>
2022-03-11 15:12:32 +0100

    removing wrong line of code

--- script.js
+++ script.js
@@ -54,7 +54,6 @@ document.querySelector('.check').addEventListener('click', function () {
 
 const resetButton = document.querySelector('.again');
 resetButton.addEventListener('click', () => {
-  alert('hello world');
   // keep the highest score if it's better than current max score
   if (
     score > highScore &&
