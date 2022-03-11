64b61ee84fc1272ebe53eced8971493d3b80542e
Aleksander Jaworski <aleck156@gmail.com>
2022-03-11 15:03:07 +0100

    complete New Game button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 20) + 1;
+const drawSecretNumber = function () {
+  return Math.floor(Math.random() * 20) + 1;
+};
+
+let secretNumber = drawSecretNumber();
 let score = 20;
+let highScore = 0;
 // document.querySelector('.number').textContent = secretNumber;
 
 let message = document.querySelector('.message');
@@ -23,6 +28,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       message.textContent = 'Too damn high!';
@@ -44,4 +50,40 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     }
   }
+});
+
+const resetButton = document.querySelector('.again');
+resetButton.addEventListener('click', () => {
+  // keep the highest score if it's better than current max score
+  if (
+    score > highScore &&
+    secretNumber === Number(document.querySelector('.guess').value)
+  ) {
+    console.log(
+      `replacing old highest score (${highScore}) with a new value (${score})`
+    );
+    highScore = score;
+    document.querySelector('.highscore').innerHTML = highScore;
+  }
+
+  // draw a new secret number
+  secretNumber = drawSecretNumber();
+  console.log(`drawing new secret number ${secretNumber}`);
+
+  // reset message
+  message.textContent = 'Start guessing...';
+
+  // restore background color
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  // restore initial value of guess number to 10
+  document.querySelector('.guess').value = 10;
+
+  // restore the initial question mark
+  document.querySelector('.number').textContent = '?';
+
+  // reset score
+  score = 20;
+  document.querySelector('.score').textContent = 20;
+  //
 });
